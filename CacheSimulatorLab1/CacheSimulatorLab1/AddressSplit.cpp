6d8c971e5fe221b7a2d452d856c76596f8d6dc6e
Cristobal Gallegos <crisgallegos14@gmail.com>
2014-10-04 16:05:46 -0600

    Added class AddressSplit

---
+++ CacheSimulatorLab1/CacheSimulatorLab1/AddressSplit.cpp
@@ -0,0 +1,9 @@
+//
+//  AddressSplit.cpp
+//  CacheSimulatorLab1
+//
+//  Created by Cristobal Gallegos on 10/4/14.
+//  Copyright (c) 2014 Cristobal Gallegos. All rights reserved.
+//
+
+#include "AddressSplit.h"
---
+++ CacheSimulatorLab1/CacheSimulatorLab1/AddressSplit.h
@@ -0,0 +1,14 @@
+//
+//  AddressSplit.h
+//  CacheSimulatorLab1
+//
+//  Created by Cristobal Gallegos on 10/4/14.
+//  Copyright (c) 2014 Cristobal Gallegos. All rights reserved.
+//
+
+#ifndef __CacheSimulatorLab1__AddressSplit__
+#define __CacheSimulatorLab1__AddressSplit__
+
+#include <stdio.h>
+
+#endif /* defined(__CacheSimulatorLab1__AddressSplit__) */
